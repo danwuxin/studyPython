5af57fcb63bb2cc4b2b786523523bb9956f27481
danwuxin <danwuxin@126.com>
2019-11-17 22:23:58 +0800

    提交代码

---
+++ com/example/day2/Day2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+# @Time : 2019/11/17 21:50 
+# @Author : danwuxin 
+# @Site :  
+# @File : Day2.py 
+# @Software: PyCharm
+print("Hello,World");
+name = "adv Test";
+
+print(name.title());
+
+print(name.upper());
+
+print(name.lower());
+
+age = 23
+message = "Happy" + str(age) +"Birthday"
+print(message);
---
+++ com/example/day2/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+# @Time : 2019/11/17 21:49 
+# @Author : danwuxin 
+# @Site :  
+# @File : __init__.py.py 
+# @Software: PyCharm
