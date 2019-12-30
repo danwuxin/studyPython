dcae9fc3ff2e068e23f4fe4a77b41bff165ddbe0
danwuxin <danwuxin@126.com>
2019-12-30 22:49:51 +0800

    提交代码

---
+++ com/example/day5/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+# @Time : 2019/12/30 22:30 
+# @Author : danwuxin 
+# @Site :  
+# @File : __init__.py.py 
+# @Software: PyCharm
---
+++ com/example/day5/study5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+# @Time : 2019/12/30 22:31 
+# @Author : danwuxin 
+# @Site :  
+# @File : study5.py 
+# @Software: PyCharm
+players =  ['charles','martina','michael','florence','eli']
+print(players[0:3])
+
+print(players[2:])
+
+players1 =players[:]
+
+print("players1",players1)
